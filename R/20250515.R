6106cd6cd40957d752c74e1fe54c128e61e8dfce
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2025-05-15 13:10:00 +0900

    Create 20250515.R

---
+++ R/20250515.R
@@ -0,0 +1,19 @@
+# 주석
+# 설명 -> 코드에 대한 설명 
+# 나에게, 나 이외의 사람들에게 코드를 설명
+
+# 변수 생성 
+a <- 'test'
+a
+print(a)
+1000 -> b
+b
+c = TRUE
+c
+# a, b, c의 데이터의 타입을 확인 
+typeof(a)
+typeof(b)
+typeof(c)
+# a 변수에 다른 데이터를 대입 
+a <- 10L
+typeof(a)
