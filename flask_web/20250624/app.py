0f19b685309fd73f6d66aeecb3ed073334908c9b
moons <ekflafg@gmail.com>
2025-06-24 13:20:17 +0900

    update

---
+++ flask_web/20250624/app.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, render_template
+import random 
+
+app = Flask(__name__)
+
+# api 생성 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/game')
+def game():
+    # 유저가 보낸 데이터를 변수에 저장 
+    _user = request.args['user_select']
+    # 컴퓨터가 가위, 바위, 보 중 랜덤 선택 
+    com_list = ['가위', '바위', '보']
+    com_select = random.choice(com_list)
+    print(f"유저가 보낸 데이터 : {_user}")
+    print(f"서버가 선택한 데이터 : {com_select}")
+    if _user == com_select:
+        result = '무승부'
+    else:
+        if _user == '가위':
+            if com_select == '보':
+                result = '승리'
+            else:
+                result = '패배'
+        elif _user == '바위':
+            if com_select == '가위':
+                result = '승리'
+            else:
+                result = '패배'
+        else:
+            if com_select == '바위':
+                result = '승리'
+            else:
+                result = '패배'
+    return result
+
+@app.route('/game_result')
+def game2():
+    # 유저가 비동기 방식으로 보낸 데이터를 변수에 저장 
+    _user = request.args['user_select']
+    # 컴퓨터가 가위, 바위, 보 중 랜덤 선택 
+    com_list = ['가위', '바위', '보']
+    com_select = random.choice(com_list)
+    print(f"유저가 보낸 데이터 : {_user}")
+    print(f"서버가 선택한 데이터 : {com_select}")
+    if _user == com_select:
+        result = '무승부'
+    else:
+        if _user == '가위':
+            if com_select == '보':
+                result = '승리'
+            else:
+                result = '패배'
+        elif _user == '바위':
+            if com_select == '가위':
+                result = '승리'
+            else:
+                result = '패배'
+        else:
+            if com_select == '바위':
+                result = '승리'
+            else:
+                result = '패배'
+    # return 데이터가 ajax에서 type 이 json으로 설정
+    # json과 같은 형태 -> dict
+    res_data = {
+        'result' : result
+    }
+    return res_data
+
+
+app.run(debug=True)
